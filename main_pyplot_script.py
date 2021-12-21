c7e136cae5f2f7a07d5d9f30a7914843fd95948b
Alexandre <Alexandre@MacBook-Pro-de-Alexandre.local>
2021-12-22 00:02:25 +0100

    Tests unitaires

--- main_pyplot_script.py
+++ main_pyplot_script.py
@@ -32,8 +32,123 @@ class LineSyntaxError(Exception):
     """
     Erreur pour indiquer un problème de format sur une certaine ligne.
     """
-    def __int__(self, line_num="None"):
-        self.line_num = line_num
+    def __int__(self, line="None"):
+        self.line = line
+
+
+def clean_lines(all_lines):
+    """
+    Fonction supprimant les valeurs doublons dans une liste de lignes d'un fichier.
+    :param - all_lines: Liste contenant les lignes du fichier texte analysé.
+    :return - clean_lines: Liste des lignes sans doublons
+    """
+    new_lines = []  # liste des lignes sans les doublons
+    first_line = True
+    for line in all_lines:
+        line = line.strip("\n")
+        line = line.strip()  # .strip() -> on enlève les espaces en trop au début et à la fine
+
+        if first_line:
+            previous_line = line
+            first_line = False
+            new_lines.append(line)
+            continue  # on ne peut pas verifier un doublon à la première ligne
+
+        # On saute l'écriture dans la nouvelle liste les lignes en doublon l'une à la suite des autres.
+        # Notamment les doubles lignes vides
+        if line == previous_line:
+            continue
+
+        new_lines.append(line)
+        previous_line = line
+
+    return new_lines
+
+def draw(main_list):
+    """
+
+    """
+    color_list = ["goldenrod", "blueviolet", "crimson", "navy", "seagreen", "steelblue", "maroon", "coral",
+                  "olivedrab", "lime", "darkorange", "aqua", "dimgray", "cornflowerblue"]
+
+    for group in main_list:
+        draw_color = color_list[randint(0, len(color_list) - 1)]
+        if len(group) == 1:
+            group[0].draw_point(markerfacecolor=draw_color)  # Quand un seul point on le dessine avec Point.draw_point()
+            continue
+        my_figure = Figure(group)
+        my_figure.draw_figure(color=draw_color)
+
+def group_figures(all_lines):
+    """
+    Fonction qui prends en paramètres une liste des lignes du fichier à analyser. Renvoie une liste de listes. La
+    fonction supprime les lignes doublons les unes à la suite des autres. Les différentes listes représentent les
+    groupements de figures. Les groupements sont déterminés par les lignes vides dans le fichier texte analysé.
+    :param - all_lines: Liste des lignes du fichier.
+    :return - grouped_points: Liste contenant une liste pour chaque groupement de points.
+    """
+    main_list = []  # liste qui contiendra les groupes
+    i = 0
+    # indice de main_list. On aura main_list = [[point1, point2],[point3],[........],[.........]]
+    # l'indice augmentera de 1 lorsqu'on tombe sur une liste vide -> début d'une nouvelle figure
+
+    maxx = 0
+    minx = 0
+    maxy = 0
+    miny = 0
+
+    new_lines = clean_lines(all_lines)
+
+    # on retire la première ligne si c'est une ligne vide
+    if new_lines[0] == "":
+        new_lines = new_lines[1:]
+
+    # on pré-construit l'array final avec les groupements de points
+    number_of_groups = new_lines.count("")
+    for j in range(number_of_groups):
+        main_list.append([])
+
+    for line in new_lines:
+        if line == "":
+            i += 1  # on commence une nouvelle sous-liste
+            continue
+
+        line = line.split()  # division en une liste
+
+        # Vérification de si la liste a la bonne taille.
+        if len(line) != 3:
+            print("Erreur taille")
+            raise LineSyntaxError(line)
+
+
+        # marche pas pour l'instant
+        # Vérification de si les valeurs dans une ligne sont justes
+        try:
+            float(line[1])
+            float(line[2])
+        except ValueError:
+            print("Erreur type")
+            raise LineSyntaxError(line)
+
+
+        line[1] = float(line[1])
+        line[2] = float(line[2])
+
+        if line[1] > maxx:
+            maxx = line[1]
+        if line[2] > maxy:
+            maxy = line[2]
+        if line[1] < minx:
+            minx = line[1]
+        if line[2] < miny:
+            miny = line[2]
+
+        # Création du point dans la sous-liste
+        main_list[i].append(Point(line[0], line[1], line[2]))
+
+    extremes = [minx, maxx, miny, maxy]
+
+    return main_list, extremes
 
 
 if __name__ == "__main__":
@@ -43,108 +158,25 @@ if __name__ == "__main__":
             lines = file.readlines()
             lines.append("\n")  # pour être sur que e fichier fini par une ligne au vide. Au pire elle sera supprimée
 
-            new_lines = []  # liste des lignes sans les doublons
-            first_line = True
-            main_list = []  # liste qui contiendra les groupes
-            i = 0
-            maxx = 0
-            minx = 0
-            maxy = 0
-            miny = 0
 
-            # indice de main_list. On aura main_list = [[point1, point2],[point3],[........],[.........]]
-            # l'indice augmentera de 1 lorsqu'on tombe sur une liste vide -> début d'une nouvelle figure
+            main_list = group_figures(lines)[0]
+            extremes = group_figures(lines)[1]
 
-            for line in lines:
-                original_line = line
-
-                line = line.strip("\n")
-                line = line.strip()  # .strip() -> on enlève les espaces en trop au début et à la fine
-
-                if first_line:
-                    previous_line = line
-                    first_line = False
-                    new_lines.append(line)
-                    continue  # on ne peut pas verifier un doublon à la première ligne
-
-                # On saute l'écriture dans la nouvelle liste les lignes en doublon l'une à la suite des autres.
-                # Notamment les doubles lignes vides
-                if line == previous_line:
-                    continue
-
-                new_lines.append(line)
-                previous_line = line
-
-            # on retire la première ligne si c'est une ligne vide
-            if new_lines[0] == "":
-                new_lines = new_lines[1:]
-
-            # on pré-construit l'array final avec les groupements de points
-            number_of_groups = new_lines.count("")
-            for j in range(number_of_groups):
-                main_list.append([])
-
-            for line in new_lines:
-                if line == "":
-                    i += 1  # on commence une nouvelle sous-liste
-                    continue
-
-                line = line.split()  # division en une liste
-
-                # Vérification de si la liste a la bonne taille.
-                if len(line) != 3:
-                    print("Erreur taille")
-                    raise LineSyntaxError
-
-                """ # marche pas pour l'instant
-                # Vérification de si les valeurs dans une ligne sont justes
-                try:
-                    print(float(line[1]))
-                    float(line[1])
-                    print(float(line[2]))
-                    float(lines[2])
-                except ValueError:
-                    print("Erreur type")
-                    raise LineSyntaxError
-                """
-
-                line[1] = float(line[1])
-                line[2] = float(line[2])
-
-                if line[1] > maxx:
-                    maxx = line[1]
-                if line[2] > maxy:
-                    maxy = line[2]
-                if line[1] < minx:
-                    minx = line[1]
-                if line[2] < miny:
-                    miny = line[2]
-
-                # Création du point dans la sous-liste
-                main_list[i].append(Point(line[0], line[1], line[2]))
 
     except FileNotFoundError:
         print("Erreur. Le fichier n'existe pas.")
     except IOError:
         print("Erreur IO.")
     except LineSyntaxError as e:
-        print("Erreur de syntaxe à la ligne {}".format(" ".join(line)))  # On indique à quelle ligne il y a  une erreur
-        # print("erreur de syntaxe dans une des lignes")
+        #print("Erreur de syntaxe à la ligne {}".format(" ".join(e.line)))  # On indique à quelle ligne il y a une erreur
+        # Pas réussi à affihcer la ligne ->  ... object has no attribute 'line'
         exit()
 
-    color_list = ["goldenrod", "blueviolet", "crimson", "navy", "seagreen", "steelblue", "maroon", "coral",
-                  "olivedrab", "lime", "darkorange", "aqua", "dimgray", "cornflowerblue"]
+    # desiin des points et figures
+    draw(main_list)
 
-    for group in main_list:
-        draw_color = color_list[randint(0, len(color_list)-1)]
-        if len(group) == 1:
-            group[0].draw_point(markerfacecolor=draw_color)  # Quand un seul point on le dessine avec Point.draw_point()
-            continue
-        my_figure = Figure(group)
-        my_figure.draw_figure(color=draw_color)
-
-    pyplot.xlim(minx-2, maxx+2)
-    pyplot.ylim(miny-2, maxy+2)
+    pyplot.xlim(extremes[0] - 2, extremes[1] + 2)
+    pyplot.ylim(extremes[2] - 2, extremes[3] + 2)
     pyplot.savefig(save_file)
     pyplot.show()
 
---
+++ test.py
@@ -0,0 +1,98 @@
+import unittest
+
+from main_pyplot_script import clean_lines, group_figures, LineSyntaxError
+
+
+class TestCleanLines(unittest.TestCase):
+    def test_clean_lines_1(self):
+        initial_lines = ["\n", "\n", "P1 2 7\n", "P2 21 2\n", "P3 2 4\n", "\n", "\n", "\n", "P4 3 3\n", "P5 9 5\n",
+                         "\n", "\n", "P6 22 1\n", "\n"]
+        excepected = ["", "P1 2 7", "P2 21 2", "P3 2 4","" "P4 3 3", "P5 9 5", "P6 22 1", ""]
+        self.assertEqual(excepected, clean_lines(initial_lines))
+
+    def test_clean_lines_2(self):
+        initial_lines = ["P1 3 4\n", "\n", "\n", "\n", "\n", "P2 3 8\n", "\n", "\n", "P3 8 5\n", "P3 8 5\n"]
+        excepected = ["P1 3 4", "", "P2 3 8", "", "P3 8 5"]
+        self.assertEqual(excepected, clean_lines(initial_lines))
+
+    def test_clean_lines_3(self):
+        initial_lines = ["\n", "\n", "P1 67 87", "P1 67 87", "P1 67 87", "P2 3 4", "\n"]
+        excepected = ["", "P1 67 87", "P2 3 4", ""]
+        self.assertEqual(excepected, clean_lines(initial_lines))
+
+    def test_clean_lines_all_empty(self):
+        initial_lines = ["\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n",]
+        excepected = [""]
+        self.assertEqual(excepected, clean_lines(initial_lines))
+
+    def test_clean_lines_all_same(self):
+        initial_lines = ["P1 3 4\n", "P1 3 4\n", "P1 3 4\n", "P1 3 4\n", "P1 3 4\n", "P1 3 4\n", ]
+        excepected = ["P1 3 4", ]
+        self.assertEqual(excepected, clean_lines(initial_lines))
+
+    def test_clean_lines_empty(self):
+        initial_lines = []
+        excepected = []
+        self.assertEqual(excepected, clean_lines(initial_lines))
+
+    # Pas vraiment d'autres cas. Vu qu'on lis d'un fichier on aura tout le temps des tring dans la liste de départ
+
+class TestGroupFigures(unittest.TestCase):
+    def test_group_figures_1(self):
+        initial_lines = ["\n", "\n", "P1 2 7\n", "P2 21 2\n", "P3 2 4\n", "\n", "\n", "\n", "P4 3 3\n", "P5 9 5\n",
+                         "\n", "\n", "P6 22 1\n", "\n"]
+        excepected = [["P1 2 7", "P2 21 2", "P3 2 4"], ["P4 3 3", "P5 9 5"], ["P6 22 1"]]
+        self.assertEqual(excepected, clean_lines(initial_lines)[0])
+
+    def test_group_figures_2(self):
+        initial_lines = ["P1 3 4\n", "\n", "\n", "\n", "\n", "P2 3 8\n", "\n", "\n", "P3 8 5\n", "P3 8 5\n"]
+        excepected =[["P1 3 4"], ["P2 3 8"], ["P3 8 5"]]
+        self.assertEqual(excepected, clean_lines(initial_lines)[0])
+
+    def test_group_figures_3(self):
+        initial_lines = ["\n", "\n", "P1 67 87", "P1 67 87", "P1 67 87", "P2 3 4", "\n"]
+        excepected = [["P1 67 87", "P1 67 87", "P1 67 87", "P2 3 4"]]
+        self.assertEqual(excepected, clean_lines(initial_lines)[0])
+
+    def test_group_figures_all_empty(self):
+        initial_lines = ["\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n", "\n",]
+        excepected = [""]
+        self.assertEqual(excepected, clean_lines(initial_lines)[0])
+
+    def test_group_figures_all_same(self):
+        initial_lines = ["P1 3 4\n", "P1 3 4\n", "P1 3 4\n", "P1 3 4\n", "P1 3 4\n", "P1 3 4\n", ]
+        excepected = [["P1 3 4"]]
+        self.assertEqual(excepected, clean_lines(initial_lines)[0])
+
+    def test_group_figures_empty(self):
+        initial_lines = []
+        excepected = []
+        self.assertEqual(excepected, clean_lines(initial_lines)[0])
+
+    def test_group_figures_float(self):
+        initial_lines = ["\n", "\n", "P1 2.4 7\n", "P2 21 2\n", "P3 2 4.88\n", "\n", "\n", "\n", "P4 3 3.1\n", "P5 9 5\n",
+                         "\n", "\n", "P6 22.09 1\n", "\n"]
+        excepected = [["P1 2.4 7", "P2 21 2", "P3 2 4.88"], ["P4 3 3.1", "P5 9 5"], ["P6 22.09 1"]]
+        self.assertEqual(excepected, clean_lines(initial_lines)[0])
+
+    def test_group_figures_non_float_error(self):
+        """
+        Teste le renvoi de l'erreur LineSyntaxError lorsqu'une des coordonnées dans une ligne n'est pas un nombre.
+        """
+        initial_lines = ["\n", "\n", "P1 2 7\n", "P2 t 2\n", "P3 2 4\n", "\n", "\n", "\n", "P4 3 3\n", "P5 9 5\n",
+                         "\n", "\n", "P6 22 1\n", "\n"]
+        self.assertRaises(LineSyntaxError)
+
+    def test_group_figures_too_much_per_line(self):
+        """
+        Teste le renvoi de l'erreur LineSyntaxError lorsqu'une des lignes possède trop d'arguments.
+        """
+        initial_lines = ["\n", "\n", "P1 2 7\n", "P2 21 2 5\n", "P3 2 4\n", "\n", "\n", "\n", "P4 3 3\n", "P5 9 5\n",
+                         "\n", "\n", "P6 22 1\n", "\n"]
+        self.assertRaises(LineSyntaxError)
+
+
+
+
+
+
